1aa86c7c1c4e336a6c4ea43281efb4990e0a02ab
Fernando <fernando.06cm@gmail.com>
2026-02-24 10:09:57 +0100

    sigo con pokemons

--- ejercicio-2.sql
+++ ejercicio-2.sql
@@ -1,4 +1,9 @@
-SELECT pokemons.id, pokemons.nombre, tipos.nombre, generacion.id
-FROM pokemons
+SELECT 
+pokemons.id AS pokedex, 
+pokemons.nombre AS pokemon,
+tipos.nombre AS tipo,
+generacion.nombre AS generacion
+FROM pokemons JOIN generacion
+ON pokemons.generacion_id = generacion.id
 JOIN pokemon_tipo
 ON pokemons.id
