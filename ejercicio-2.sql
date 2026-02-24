127236a518707d1bac97935a570c62c310baeade
Fernando <fernando.06cm@gmail.com>
2026-02-24 10:16:35 +0100

    terminado 2

--- ejercicio-2.sql
+++ ejercicio-2.sql
@@ -6,4 +6,7 @@ generacion.nombre AS generacion
 FROM pokemons JOIN generacion
 ON pokemons.generacion_id = generacion.id
 JOIN pokemon_tipo
-ON pokemons.id
+ON pokemons.id = pokemon_tipo.pokemon_id
+JOIN tipos
+ON pokemon_tipo.tipo_id = tipos.id
+ORDER BY pokemons.id
